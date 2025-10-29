81382136b72284820951f116ec389a4e7c1ec07b
Chinmay <chinmaybarik214@gmail.com>
2025-10-29 11:39:08 +0530

    Fix bugs with regex and dotenv

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({path: "../../.env"});
-
+dotenv.config({path: "./.env"});
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("Error connecting to MongoDB", error);
+    // console.error("Error connecting to MongoDB", error);
     process.exit(1); // exit with failure
   }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
 import path from "path";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -17,7 +15,7 @@ app.use(express.json());
 app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
